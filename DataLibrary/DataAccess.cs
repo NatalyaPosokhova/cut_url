452cfe0a9a08c110e9e5f7d5374aed6bf2e98ebf
Natalya <Natalya.Posokhova@gmail.com>
2020-10-22 14:29:36 +0500

    Dapper and MySql were added into environment

---
+++ DataLibrary/DataAccess.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Dapper;
+using MySql.Data.MySqlClient;
+
+namespace DataLibrary
+{
+    class DataAccess : IDataAccess
+    {
+
+    }
+}
