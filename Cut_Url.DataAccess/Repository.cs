9db8d04a0c000ce47ee7a814cef40be191ea8800
NatalyaPosokhova <natalya.posokhova@gmail.com>
2021-01-17 00:12:38 -0800

    add constructor to repository

---
+++ Cut_Url.DataAccess/Repository.cs
@@ -0,0 +1,35 @@
+﻿using Cut_URL.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cut_Url.DataAccess
+{
+    class Repository : IRepository
+    {
+        private ShortcutUrlData urlData;
+        public Repository()
+        {
+            urlData = new ShortcutUrlData();
+        }
+        public void AddShortUrlData(string userId, string shortUrl, string longUrl)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ShortcutUrlData GetUrlDataByShortUrl(string shortUrl)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsShortUrlExists(string shortUrl)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveUrlData(ShortcutUrlData urlData)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
