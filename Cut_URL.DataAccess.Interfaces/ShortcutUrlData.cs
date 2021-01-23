b98b8baf2020ced1d1e9910390c8f8040ff6bad9
NatalyaPosokhova <natalya.posokhova@gmail.com>
2021-01-23 13:00:16 -0800

    +Add Equals method to compare two objects

--- Cut_URL.DataAccess.Interfaces/ShortcutUrlData.cs
+++ Cut_URL.DataAccess.Interfaces/ShortcutUrlData.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Cut_URL.DataAccess
 {
-    public class ShortcutUrlData
+    public class ShortcutUrlData : IEquatable<ShortcutUrlData>
     {
         [Key]
         public int Id { get; set; }
@@ -12,5 +13,33 @@ namespace Cut_URL.DataAccess
         public string LongUrl { get; set; }
         public string ShortUrl { get; set; }
         public string UserId { get; set; }
+
+        public bool Equals(ShortcutUrlData anotherUrlData)
+        {
+            if (anotherUrlData == null)
+                return false;
+
+            return this.TransferQuantity.Equals(anotherUrlData.TransferQuantity) &&
+                (
+                    object.ReferenceEquals(this.LongUrl, anotherUrlData.LongUrl) ||
+                    this.LongUrl != null &&
+                    this.LongUrl.Equals(anotherUrlData.LongUrl)
+                ) &&
+                (
+                    object.ReferenceEquals(this.ShortUrl, anotherUrlData.ShortUrl) ||
+                    this.ShortUrl != null &&
+                    this.ShortUrl.Equals(anotherUrlData.ShortUrl)
+                ) &&
+                (
+                    object.ReferenceEquals(this.UserId, anotherUrlData.UserId) ||
+                    this.UserId != null &&
+                    this.UserId.Equals(anotherUrlData.UserId)
+                );
+        }
+
+        //public bool Equals([AllowNull] ShortcutUrlData obj)
+        //{
+        //    return this.Equals(obj);
+        //}
     }
 }
